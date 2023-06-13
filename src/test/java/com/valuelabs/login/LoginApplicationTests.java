269d643373aeaeb0d1a2919234eff529868bf90b
pravat.surya <pravat.surya25@gmail.com>
2023-06-13 23:20:51 +0530

    Empty project

---
+++ src/test/java/com/valuelabs/login/LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valuelabs.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
